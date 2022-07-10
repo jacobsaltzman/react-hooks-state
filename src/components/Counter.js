57d3bcdfff4376a8a8ff83ee6f441c91975b46b1
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-07-10 13:15:43 -0700

    commit tree b8fa913ea0608a27cfe383dc9912395aa9bca6e8

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,6 +5,7 @@ function Counter() {
 
   function increment() {
     setCount(count + 1);
+    console.log("click")
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
